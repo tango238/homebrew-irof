79cea801040fe16a35c614b53e5ec65f93cdbba0
Go Tanaka <tanago3@gmail.com>
2012-12-21 01:51:46 +0900

    extends Formula

--- irof.rb
+++ irof.rb
@@ -3,7 +3,7 @@ require 'formula'
 # Documentation: https://github.com/mxcl/homebrew/wiki/Formula-Cookbook
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 
-class Irof 
+class Irof < Formula
   url 'https://github.com/tango238/homebrew-irof/tarball/master'
   version '0'
   #sha1 'ca0ec4a469f998c79ad3c113dd81359080fdc8dc'

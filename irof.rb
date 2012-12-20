d405a9c8e3a48d27a16670414d26b539d3c16931
Go Tanaka <tanago3@gmail.com>
2012-12-21 01:23:47 +0900

    remove extends class.

--- irof.rb
+++ irof.rb
@@ -3,12 +3,13 @@ require 'formula'
 # Documentation: https://github.com/mxcl/homebrew/wiki/Formula-Cookbook
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 
-class Irof < Formula
-  homepage ''
+class Irof 
   url 'https://github.com/tango238/homebrew-irof/tarball/master'
-  sha1 'ca0ec4a469f998c79ad3c113dd81359080fdc8dc'
+  version '0'
+  #sha1 'ca0ec4a469f998c79ad3c113dd81359080fdc8dc'
 
   def install
+    print('installing..')
   end
 
   def test
